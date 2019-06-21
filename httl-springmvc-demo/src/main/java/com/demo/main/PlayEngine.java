a59869f27d08236067e1f37659c87f61ecaa33cf
Wei <aweisw@gmail.com>
2019-06-21 17:28:03 +0800

    play engine in command line

---
+++ httl-springmvc-demo/src/main/java/com/demo/main/PlayEngine.java
@@ -0,0 +1,28 @@
+package com.demo.main;
+
+import httl.Engine;
+import httl.Template;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayEngine {
+
+	public static void main(String[] args) throws Throwable {
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("user", "wei");
+		parameters.put("books", "ag");
+		
+		//due to bug when parsing windows path, copy config file to class path
+		Engine engine = Engine.getEngine("httl.properties");
+		
+		//need config
+		//loaders=httl.spi.loaders.FileLoader
+		//template.directory=
+		Template template = engine.getTemplate("playengine.httl");
+		
+		//if only xxx_write.java WriterTemplate.doRenderStream throws UnsupportedOperationException
+		template.render(parameters, System.out);
+	}
+
+}
